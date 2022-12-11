13dbe7a8e7fa5e0e67c92b6bae05e5875c747392
balanagammal_murugan <balanagammalm@sahaj.ai>
2022-12-11 21:44:00 +0530

    Add docker compose to spin up database

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const database = mysql.createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    user: process.env.MYSQLDB_USER,
+    password: process.env.MYSQLDB_ROOT_PASSWORD,
+    database: process.env.MYSQLDB_DATABASE
 });
 
 module.exports = database;
